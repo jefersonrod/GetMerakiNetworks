3b11df525d0a0ba8008bf92c7e4a10f41fc5c81f
Jeferson <jefersonrod@users.noreply.github.com>
2018-11-14 18:50:37 -0200

    Add files via upload

---
+++ GetMerakiNetworks/Serialization/Xml/IXmlSerializer.cs
@@ -0,0 +1,13 @@
+﻿using RestSharp.Serializers;
+
+namespace RestSharp.Serialization.Xml
+{
+    public interface IXmlSerializer : ISerializer
+    {
+        string RootElement { get; set; }
+
+        string Namespace { get; set; }
+
+        string DateFormat { get; set; }
+    }
+}
